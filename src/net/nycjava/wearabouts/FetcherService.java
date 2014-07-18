527dbc57313d380a98b63585b7323d516cf29f1e
codeledger <morrison.chang@gmail.com>
2014-07-17 23:30:38 -0400

    Added missing FetcherService.java

---
+++ src/net/nycjava/wearabouts/FetcherService.java
@@ -0,0 +1,34 @@
+package net.nycjava.wearabouts;
+
+import java.util.List;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class FetcherService extends IntentService {
+
+	static private final String TAG = "WearAbouts:FetcherService";
+
+	public FetcherService() {
+		super("FetcherService");
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void onHandleIntent(Intent arg0) {
+		  Log.d(TAG,"onHandleIntent-createGeo");
+	      new Thread(new Runnable() {
+				
+				@Override
+				public void run() {
+					List<Event> events = new LocalEventsFetcher().getLocalEvents();
+					
+					new GeoFenceCreator().createGeoFences(getApplicationContext(), events);
+					
+					// TODO release wake lock here
+				}
+			}).start();
+	}
+}
